7d0f18e296fef89c82143b08a62e677e809fbf45
Felipe Espinoza <fespinozacast@gmail.com>
2013-04-22 14:11:30 -0400

    ADD: useful computed properties to actors

--- app/assets/javascripts/models/actor.js
+++ app/assets/javascripts/models/actor.js
@@ -3,4 +3,17 @@ App.Actor = DS.Model.extend({
   x: DS.attr('number'),
   y: DS.attr('number'),
   social_network: DS.belongsTo('App.SocialNetwork'),
+  radius: 20,
+  cx: function () {
+    return this.get('x') + 60;
+  }.property('x'),
+  cy: function () {
+    return this.get('y') + 20;
+  }.property('y'),
+  text_x: function () {
+    return this.get('x');
+  }.property('x'),
+  text_y: function () {
+    return this.get('y') + 60;
+  }.property('y'),
 });
