acfd267c91ec4c1bfefe36da6979a0434fb46c16
Felipe Espinoza <fespinozacast@gmail.com>
2013-04-24 15:37:22 -0400

    ADD: insert actors with d3

--- app/assets/javascripts/views/social_network_view.js
+++ app/assets/javascripts/views/social_network_view.js
@@ -9,3 +9,37 @@ App.SocialNetworkView = Ember.View.extend({
     this.controller.send('addActor', coords.x, coords.y);
   }
 });
+
+
+App.ActorsView = Ember.View.extend({
+  didInsertElement: function () {
+    var view = this;
+    this.get('controller.content').on('didLoad', function () {
+      view.insertSVGcontent();
+    });
+  },
+  insertSVGcontent: function () {
+    console.log("insert svg content");
+
+    var svg = d3.select("#graph_canvas");
+
+    var data = this.get('controller.content').toArray();
+
+    var node = svg.selectAll("g")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "actor")
+
+    var circle = node.append('circle')
+    .attr('r', function(d) { return d.get('radius'); })
+    .attr('cx', function(d) { return d.get('cx'); })
+    .attr('cy', function(d) { return d.get('cy'); });
+
+    var text = node.append('text')
+    .text(function(d) { return d.get('name'); })
+    .attr('x', function(d) { return d.get('text_x') })
+    .attr('y', function(d) { return d.get('text_y') })
+    .attr('text-anchor', 'middle');
+  },
+});
