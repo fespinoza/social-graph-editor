7145acca46ec383d8da9cdfb4d9e85842f20d18e
Felipe Espinoza <fespinozacast@gmail.com>
2013-04-29 14:55:38 -0400

    ADD: persisted dragging to actor svg rep.
    Now you can drag actors svg representation
    (circle + text) and they will be persisted
    after finished dragging

--- app/assets/javascripts/views/social_network_view.js
+++ app/assets/javascripts/views/social_network_view.js
@@ -19,33 +19,58 @@ App.ActorsView = Ember.View.extend({
   didInsertElement: function () {
     var view = this;
     this.get('controller.content').on('didLoad', function () {
-      view.insertSVGcontent();
+      view.renderSVG();
     });
   },
-  insertSVGcontent: function () {
+  renderSVG: function () {
     console.log("insert svg content");
+    var view = this;
     var svg = d3.select("#graph_canvas");
     var data = this.get('controller.content').toArray();
+    
+    // define dragging behavior
+    var draggable = d3.behavior.drag()
+    .on('dragstart', function (d) {
+      console.log("drag start for "+d.get('id'));
+    })
+    .on('drag', function (d) {
+      // move the coordinates of the actor
+      d.set('x', d.get('x') + d3.event.dx);  
+      d.set('y', d.get('y') + d3.event.dy);  
+      view.tick();
+    })
+    .on('dragend', function (d) {
+      console.log('drag end for '+d.get('id'));
+      // update position changes to the server
+      d.get('store').commit();
+    });
 
     // set the text element to handle
-    var text   = svg.selectAll("text").data(data);
-    var circle = svg.selectAll("circle").data(data);
+    this.text   = svg.selectAll("text").data(data);
+    this.circle = svg.selectAll("circle").data(data);
 
     // enter state: append text
-    text.enter().append("text").attr("text-anchor", "middle");
-    circle.enter().append("circle").attr("r", function(d) { return d.get('radius'); });
+    this.text.enter().append("text")
+      .attr("text-anchor", "middle")
+      .call(draggable);
+    this.circle.enter().append("circle")
+      .attr("r", function(d) { return d.get('radius'); })
+      .call(draggable);
 
-    // update state: update text content and coordinates
-    text.text(function(d){ return d.get('name'); })
-    .attr("x", function(d) { return d.get('text_x') })
-    .attr("y", function(d) { return d.get('text_y') });
-
-    circle.attr('cx', function(d) { return d.get('cx'); })
-    circle.attr('cy', function(d) { return d.get('cy'); })
+    this.tick();
 
     // exit state: remove unused text
-    text.exit().remove();
-    circle.exit().remove();
+    this.text.exit().remove();
+    this.circle.exit().remove();
 
-  }.observes('controller.length', 'controller.@each.name'),
+  }.observes('controller.length'),
+  tick: function () {
+    // update state: update text content and coordinates
+    this.text.text(function(d){ return d.get('name'); })
+      .attr("x", function(d) { return d.get('text_x') })
+      .attr("y", function(d) { return d.get('text_y') });
+
+    this.circle.attr('cx', function(d) { return d.get('cx'); })
+      .attr('cy', function(d) { return d.get('cy'); });
+  }.observes('controller.@each.name')
 });
