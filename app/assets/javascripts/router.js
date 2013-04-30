03fd845db2b8cf60d9c1577c0800e0a353d517b8
Felipe Espinoza <fespinozacast@gmail.com>
2013-04-30 16:19:40 -0400

    ADD: default redirection to the social network list

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -3,6 +3,11 @@ App.Router.map(function() {
   this.route("social_network", { path: "social_networks/:social_network_id" });
 });
 
+App.IndexRoute = Ember.Route.extend({
+  redirect: function () {
+    return this.transitionTo('social_networks');
+  },
+});
 
 App.SocialNetworksRoute = Ember.Route.extend({
   model: function() {
