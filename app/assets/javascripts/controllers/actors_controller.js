20dd865ef4e884f04b55a7501e64bb67bc5473df
Felipe Espinoza <fespinozacast@gmail.com>
2013-04-30 15:35:56 -0400

    ADD: accept only one new created actor at a time

--- app/assets/javascripts/controllers/actors_controller.js
+++ app/assets/javascripts/controllers/actors_controller.js
@@ -1,11 +1,20 @@
 App.ActorsController = Ember.ArrayController.extend({
   currentActor: null,
+  currentNewActor: null,
+
   add: function(x, y) {
+    if (this.get('currentNewActor')) {
+      console.log("there was a previous new actor");
+      this.get('currentNewActor').deleteRecord();
+    }
+
     var actor = App.Actor.createRecord({name: "New Actor", x: x, y: y});
     actor.set('isEditing', true);
     this.set('currentActor', actor);
+    this.set('currentNewActor', actor);
     this.get('content').pushObject(actor);
   },
+
   delete: function (actor) {
     if (confirm("Are you sure to delete the actor "+actor.get('name')+"?")) {
       // set currentActor null with this
