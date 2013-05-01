8222b6b4c0d20edaf63c466f0e4b5bd82762c46a
Felipe Espinoza <fespinozacast@gmail.com>
2013-05-01 10:54:14 -0400

    FIX: refactor cancel button in new form

--- app/assets/javascripts/controllers/social_networks_controller.js
+++ app/assets/javascripts/controllers/social_networks_controller.js
@@ -8,6 +8,9 @@ App.SocialNetworksController = Ember.ArrayController.extend({
 });
 
 App.SocialNetworksNewController = Ember.ObjectController.extend({
+  cancel: function () {
+    this.transitionToRoute('social_networks');
+  },
   save: function () {
     this.get('store').commit();
     this.transitionToRoute('social_networks');
