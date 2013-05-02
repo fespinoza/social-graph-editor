224cc514f14472bb02d50f533209314764cae490
Felipe Espinoza <fespinozacast@gmail.com>
2013-05-02 16:11:48 -0400

    ADD: center circle and text to relations

--- app/assets/javascripts/models/relation.js
+++ app/assets/javascripts/models/relation.js
@@ -2,4 +2,18 @@ App.Relation = DS.Model.extend({
   name: DS.attr('string'),
   actors: DS.hasMany('App.Actor'),
   social_network: DS.belongsTo('App.SocialNetwork'),
+  x: function() {
+    var actors = this.get('actors').toArray();
+    var sum = actors.reduce(function (sum, actor) {
+      return sum + actor.get('cx');
+    }, 0);
+    return sum / actors.length;
+  }.property('actors.@each.x'),
+  y: function() {
+    var actors = this.get('actors').toArray();
+    var sum = actors.reduce(function (sum, actor) {
+      return sum + actor.get('cy');
+    }, 0);
+    return sum / actors.length;
+  }.property('actors.@each.y'),
 });
--- app/assets/javascripts/views/relations_view.js
+++ app/assets/javascripts/views/relations_view.js
@@ -19,18 +19,30 @@ App.RelationsView = Ember.View.extend({
 
     // set the svg element which will habdle the data
     this.line = svg.selectAll("line").data(data);
+    this.circle = svg.selectAll("circle.relation").data(data);
+    this.text = svg.selectAll("text.relation").data(data);
 
     // enter state: when new lines are created
     this.line.enter().append("line")
       .attr("stroke-width", 2)
       .attr("stroke", "red")
       .on('click', this.relationClick);
+    this.circle.enter().append("circle")
+      .attr('class', 'relation')
+      .attr('r', 2)
+      .attr('fill', "blue")
+    this.text.enter()
+      .append('text')
+      .attr('class', 'relation')
+      .attr('text-anchor', 'middle')
 
     // update state: set line coordinates
     this.tick();
 
     // exit state: when relations are deleted
     this.line.exit().remove();
+    this.circle.exit().remove();
+    this.text.exit().remove();
   },
   relationClick: function(d) {
     d3.event.stopPropagation();
@@ -44,6 +56,13 @@ App.RelationsView = Ember.View.extend({
         .attr("y1", function(d) { return d.get('actors').toArray()[0].get('cy'); })
         .attr("x2", function(d) { return d.get('actors').toArray()[1].get('cx'); })
         .attr("y2", function(d) { return d.get('actors').toArray()[1].get('cy'); });
+      this.circle
+        .attr('cx', function(d) { return d.get('x'); })
+        .attr('cy', function(d) { return d.get('y'); });
+      this.text
+        .text(function(d) { return d.get('name');})
+        .attr('x', function(d) { return d.get('x'); })
+        .attr('y', function(d) { return d.get('y') + 20; });
     }
   },
 });
