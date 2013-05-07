bb6b2b95e48aa62f726bf8b32184c26ef3bed09d
Felipe Espinoza <fespinozacast@gmail.com>
2013-05-07 12:26:01 -0400

    ADD: test to delete actor relation record when deleting a relation

--- app/models/actor_relation.rb
+++ app/models/actor_relation.rb
@@ -1,4 +1,5 @@
 class ActorRelation < ActiveRecord::Base
+  set_table_name "actors_relations"
   attr_accessible :actor, :relation
   belongs_to :actor
   belongs_to :relation
--- spec/models/actor_relation_spec.rb
+++ spec/models/actor_relation_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe ActorRelation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is deleted when the relation is delete" do
+    social_network = create(:social_network)
+    actors = [
+      create(:actor, name: "actor 1", social_network: social_network),
+      create(:actor, name: "actor 2", social_network: social_network),
+    ]
+    relation = build(:relation, name: "hired", social_network: social_network)
+    relation.actors = actors
+    relation.save
+
+    expect(ActorRelation.count).to eq(2)
+    relation.destroy
+    expect(ActorRelation.count).to eq(0)
+  end
 end
