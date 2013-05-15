807290113b873cc1ae584394aaaee8cc09996888
Felipe Espinoza <fespinozacast@gmail.com>
2013-05-15 10:58:36 -0400

    ADD: scale and translation to front SN model

--- app/assets/javascripts/views/social_network_view.js
+++ app/assets/javascripts/views/social_network_view.js
@@ -1,14 +1,26 @@
 App.SocialNetworkView = Ember.View.extend({
   didInsertElement: function () {
+    var view = this;
     var canvas = d3.select("#graph_canvas");
-    var root = canvas.append("g").attr("class", "root");
+    this.root = canvas.append("g").attr("class", "root");
+    this.socialNetwork = this.get('controller.content');
+
     var zoom = d3.behavior.zoom()
       .on("zoom", function () {
-        root.attr(
-          "transform",
-          "translate("+d3.event.translate.join(',')+") scale("+d3.event.scale+")"
-        );
+        view.socialNetwork.set('scale', d3.event.scale);
+        view.socialNetwork.set( 'translation_x', d3.event.translate[0]);
+        view.socialNetwork.set( 'translation_y', d3.event.translate[1]);
+        view.tick();
       });
     canvas.call(zoom);
   },
+
+  tick: function() {
+    this.root.attr(
+      "transform",
+      "translate("+this.socialNetwork.translationString()+") " +
+      "scale("+this.socialNetwork.get('scale')+")"
+    );
+  },
+
 });
--- app/serializers/social_network_serializer.rb
+++ app/serializers/social_network_serializer.rb
@@ -1,5 +1,5 @@
 class SocialNetworkSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :name
+  attributes :id, :name, :scale, :translation_x, :translation_y
   has_many :nodes
 end
