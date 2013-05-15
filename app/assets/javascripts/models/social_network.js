d99d7a3c2f7f99b86905e6b4ec70a039cf43f1c1
Felipe Espinoza <fespinozacast@gmail.com>
2013-05-15 15:12:41 -0400

    ADD: default values to Social Network

--- app/assets/javascripts/models/social_network.js
+++ app/assets/javascripts/models/social_network.js
@@ -1,8 +1,8 @@
 App.SocialNetwork = DS.Model.extend({
   name: DS.attr('string'),
-  scale: DS.attr('number'),
-  translation_x: DS.attr('number'),
-  translation_y: DS.attr('number'),
+  scale: DS.attr('number', { defaultValue: 1 }),
+  translation_x: DS.attr('number', { defaultValue: 0 }),
+  translation_y: DS.attr('number', { defaultValue: 0 }),
   nodes: DS.hasMany('App.Node'),
   families: DS.hasMany('App.Family'),
   translationString: function() {
