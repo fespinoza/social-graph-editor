f1843d195b434ab6945304db694738beb0a81686
Felipe Espinoza <fespinozacast@gmail.com>
2013-05-15 11:09:34 -0400

    ADD: persisting and restoring the SN scale and translation

--- app/assets/javascripts/views/social_network_view.js
+++ app/assets/javascripts/views/social_network_view.js
@@ -4,6 +4,7 @@ App.SocialNetworkView = Ember.View.extend({
     var canvas = d3.select("#graph_canvas");
     this.root = canvas.append("g").attr("class", "root");
     this.socialNetwork = this.get('controller.content');
+    this.persistChangesInterval = null;
 
     var zoom = d3.behavior.zoom()
       .on("zoom", function () {
@@ -11,8 +12,16 @@ App.SocialNetworkView = Ember.View.extend({
         view.socialNetwork.set( 'translation_x', d3.event.translate[0]);
         view.socialNetwork.set( 'translation_y', d3.event.translate[1]);
         view.tick();
+
+        // reset the timeout to persist the social network changes
+        window.clearTimeout(view.persistChangesInterval);
+        view.persistChangesInterval = window.setTimeout(function () {
+          console.log("persisting SN changes");
+          view.socialNetwork.get('store').commit();
+        }, 1000);
       });
     canvas.call(zoom);
+    this.tick();
   },
 
   tick: function() {
