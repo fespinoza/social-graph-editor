0555d879ae017f4ba2a53f0026f7decbd4b9efa2
Felipe Espinoza <fespinozacast@gmail.com>
2013-05-15 10:59:39 -0400

    ADD: change SN scale and translation on zooming/panning and update canvas

--- app/assets/javascripts/models/social_network.js
+++ app/assets/javascripts/models/social_network.js
@@ -1,4 +1,10 @@
 App.SocialNetwork = DS.Model.extend({
   name: DS.attr('string'),
-  nodes: DS.hasMany('App.Node')
+  scale: DS.attr('number'),
+  translation_x: DS.attr('number'),
+  translation_y: DS.attr('number'),
+  nodes: DS.hasMany('App.Node'),
+  translationString: function() {
+    return this.get('translation_x') + ", " + this.get('translation_y');
+  },
 });
