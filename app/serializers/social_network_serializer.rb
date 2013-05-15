24c93b434b62bcd0384246acf8d68c917c654253
Felipe Espinoza <fespinozacast@gmail.com>
2013-05-15 15:12:09 -0400

    FIX: social network serializer duplicate has_many problem

--- app/serializers/social_network_serializer.rb
+++ app/serializers/social_network_serializer.rb
@@ -1,6 +1,5 @@
 class SocialNetworkSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :name, :scale, :translation_x, :translation_y
-  has_many :nodes
-  has_many :families
+  has_many :nodes, :families
 end
