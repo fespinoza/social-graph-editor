e0aad0467dd9f91c3f3653280cd64aff75458974
Felipe Espinoza <fespinozacast@gmail.com>
2013-06-03 14:49:41 -0400

    FIX: adding family to node is optional now

--- app/assets/javascripts/controllers/node_controller.js
+++ app/assets/javascripts/controllers/node_controller.js
@@ -1,8 +1,10 @@
 App.NodeController = Ember.ObjectController.extend({
   save: function () {
     // add family from the form ID
-    family = App.Family.find(this.get('content.family_id'));
-    family.get('nodes').pushObject(this.get('content'));
+    if (this.get('content.family_id')) {
+      family = App.Family.find(this.get('content.family_id'));
+      family.get('nodes').pushObject(this.get('content'));
+    }
     this.set('isEditing', false);
     this.get('store').commit();
     $("#graph_canvas").trigger('nodeUpdate');
