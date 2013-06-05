5a5dd926d9db4abd899e9ffc28a8e8cbcd4fb512
Felipe Espinoza <fespinozacast@gmail.com>
2013-06-05 17:08:08 -0400

    FIX: rm isSelected isEditing from node and style node form

--- app/assets/javascripts/controllers/node_controller.js
+++ app/assets/javascripts/controllers/node_controller.js
@@ -5,12 +5,7 @@ App.NodeController = Ember.ObjectController.extend({
       family = App.Family.find(this.get('content.family_id'));
       family.get('nodes').pushObject(this.get('content'));
     }
-    this.set('isEditing', false);
     this.get('store').commit();
     $("#graph_canvas").trigger('nodeUpdate');
-  },
-
-  toggleEditing: function () {
-    this.set('isEditing', !this.get('isEditing'));
   },
 });
