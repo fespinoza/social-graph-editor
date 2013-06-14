22b20d54ddba9e91fc3924b8b9fb74190eb669fd
Felipe Espinoza <fespinozacast@gmail.com>
2013-06-14 15:27:41 -0400

    FIX: access to node_ids, family_ids and role_ids attrs in social_network

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -1,5 +1,5 @@
 class SocialNetwork < ActiveRecord::Base
-  attr_accessible :name, :scale, :translation_x, :translation_y
+  attr_accessible :name, :scale, :translation_x, :translation_y, :node_ids, :family_ids, :role_ids
   has_many :nodes
   has_many :families
   has_many :roles
