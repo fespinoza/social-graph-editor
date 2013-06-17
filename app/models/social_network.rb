9469d04632e2278739da31c67eb57bddb6a7c85f
Felipe Espinoza <fespinozacast@gmail.com>
2013-06-17 13:17:28 -0400

    FIX: add dependent destroy to social_network's has many relations

--- app/models/social_network.rb
+++ app/models/social_network.rb
@@ -1,6 +1,6 @@
 class SocialNetwork < ActiveRecord::Base
   attr_accessible :name, :scale, :translation_x, :translation_y, :node_ids, :family_ids, :role_ids
-  has_many :nodes
-  has_many :families
-  has_many :roles
+  has_many :nodes, dependent: :destroy
+  has_many :families, dependent: :destroy
+  has_many :roles, dependent: :destroy
 end
