de5d25e916068152e3e9280a41d5f6aaffb78e75
Felipe Espinoza <fespinozacast@gmail.com>
2013-06-24 12:28:52 -0400

    ADD: api entry point for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   respond_to :json
 
+  def login
+    params[:user][:password] = User.encript(params[:user][:password])
+    respond_with User.where(params[:user]).first
+  end
+
   def show
     respond_with User.find(params[:id]) 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 EmberSge::Application.routes.draw do
-  resources :users, only: [:create, :show]
+  resources :users, only: [:create, :show, :login] do
+    post :login, on: :collection
+  end
   resources :social_networks
   resources :nodes
   resources :families
