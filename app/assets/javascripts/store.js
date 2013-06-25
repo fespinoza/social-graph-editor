92bb562544915111ff1b704ba1d29bc75926521a
Felipe Espinoza <fespinozacast@gmail.com>
2013-06-25 13:49:59 -0400

    ADD: api authentication with token and front-end send token for requests

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,21 +1,29 @@
-DS.RESTAdapter.configure("plurals", { family: "families" });
-DS.RESTAdapter.configure("plurals", { node_attribute: "node_attributes" });
-App.Store = DS.Store.extend({
-  adapter: DS.RESTAdapter.extend({
-    serializer: DS.RESTSerializer.extend({
-      addHasMany: function (hash, record, key, relationship) {
-        var type = record.constructor,
-            name = relationship.key,
-            serializedHasMany = [], 
-            manyArray, embeddedType;
+App.AuthenticatedRESTAdapter = DS.RESTAdapter.extend({
+  ajax: function(url, type, hash) {
+    hash         = hash || {};
+    hash.headers = hash.headers || {};
+    hash.headers['HTTP_X_API_TOKEN'] = localStorage.token;
+    return this._super(url, type, hash);
+  },
+  serializer: DS.RESTSerializer.extend({
+    addHasMany: function (hash, record, key, relationship) {
+      var type = record.constructor,
+          name = relationship.key,
+          serializedHasMany = [], 
+          manyArray, embeddedType;
 
-        manyArray = record.get(name);
-        manyArray.forEach(function (record) {
-          serializedHasMany.push(record.get('id'));
-        }, this);
+      manyArray = record.get(name);
+      manyArray.forEach(function (record) {
+        serializedHasMany.push(record.get('id'));
+      }, this);
 
-        hash[this.singularize(name) + '_ids'] = serializedHasMany;
-      }
-    }),
+      hash[this.singularize(name) + '_ids'] = serializedHasMany;
+    }
   }),
+});
+App.AuthenticatedRESTAdapter.configure("plurals", { family: "families" });
+App.AuthenticatedRESTAdapter.configure("plurals", { node_attribute: "node_attributes" });
+
+App.Store = DS.Store.extend({
+  adapter: App.AuthenticatedRESTAdapter.extend({}),
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_filter :authenticate
   respond_to :json
 
   def login
