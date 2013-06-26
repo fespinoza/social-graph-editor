a203ef01024e88a0d281fc5374a9a7f653a3068c
Felipe Espinoza <fespinozacast@gmail.com>
2013-06-26 16:30:02 -0400

    ADD: backend: user has access to nodes, families, roles and node attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :token
 
-  # TODO: email has to be unique
+  validates :email, uniqueness: true
 
   before_create :encript_password
   before_create :generate_access_token
 
   has_many :social_networks
+  has_many :nodes, through: :social_networks
+  has_many :families, through: :social_networks
+  has_many :roles, through: :social_networks
+  has_many :node_attributes, through: :nodes
 
   def self.encript(password)
     Digest::SHA2.hexdigest("EMBERAPPSGE123" + password)
