ea0d15b717e5df26849484bea6b3c43e868c01c1
Felipe Espinoza <fespinozacast@gmail.com>
2013-07-02 18:02:34 -0400

    FIX: vocabulary and rdf serialization process

--- app/serializers/social_network_rdf_serializer.rb
+++ app/serializers/social_network_rdf_serializer.rb
@@ -11,12 +11,15 @@ class SocialNetworkRDFSerializer
   def to_rdf
     base = options[:base_uri]
     RDF::Writer.for(:n3).buffer(options) do |writer|
-      writer << RDF::Statement.new(@social_network.uri(base), prefix(:foaf, :name), @social_network.name)
+
+      # TODO: add SN description here
+      writer << RDF::Statement.new(@social_network.uri(base), prefix(:rdf, :type), sn(:socialNetwork))
+      writer << RDF::Statement.new(@social_network.uri(base), sn(:name), @social_network.name)
 
       @social_network.nodes.each do |node|
         node_uri = node.uri(base)
-        writer << RDF::Statement.new(node_uri, prefix(:rdf, :type), sn(:node))
-        writer << RDF::Statement.new(node_uri, prefix(:foaf, :name), node.name)
+        writer << RDF::Statement.new(node_uri, prefix(:rdf, :type), sn(node.kind.downcase))
+        writer << RDF::Statement.new(node_uri, sn(:name), node.name)
         writer << RDF::Statement.new(node_uri, sn(:kind), node.kind)
 
         if visual_data?
@@ -37,14 +40,15 @@ class SocialNetworkRDFSerializer
       @social_network.roles.each do |role|
         role_uri = role.uri(base)
         writer << RDF::Statement.new(role_uri, prefix(:rdf, :type), sn(:role))
-        writer << RDF::Statement.new(role_uri, prefix(:foaf, :name), role.name)
-        writer << RDF::Statement.new(role.actor.uri(base), role_uri, role.relation.uri(base))
+        writer << RDF::Statement.new(role_uri, sn(:name), role.name)
+        writer << RDF::Statement.new(role.actor.uri(base), sn(:participatesAs), role_uri)
+        writer << RDF::Statement.new(role_uri, sn(:inRelation), role.relation.uri(base))
       end
 
       @social_network.families.each do |family|
         family_uri = family.uri(base)
         writer << RDF::Statement.new(family_uri, prefix(:rdf, :type), sn(:family))
-        writer << RDF::Statement.new(family_uri, prefix(:foaf, :name), family.name)
+        writer << RDF::Statement.new(family_uri, sn(:name), family.name)
         writer << RDF::Statement.new(family_uri, sn(:kind), family.kind)
 
         if visual_data?
@@ -72,7 +76,6 @@ class SocialNetworkRDFSerializer
       prefixes: {
         rdf: RDF.to_uri,
         rdfs: RDF::RDFS.to_uri,
-        foaf: RDF::FOAF.to_uri,
         sn: RDF::URI("#{@social_network.vocabulary}#"),
       }
     }
