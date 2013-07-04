e8dac1d94cfd600621d9651817cc298316e6d22d
Felipe Espinoza <fespinozacast@gmail.com>
2013-07-04 10:44:03 -0400

    ADD: export buttons

--- app/assets/javascripts/controllers/social_network_controller.js
+++ app/assets/javascripts/controllers/social_network_controller.js
@@ -2,4 +2,28 @@ App.SocialNetworkController = Ember.ObjectController.extend({
   changeMode: function(newMode) {
     this.set('content.currentMode', newMode);
   },
+
+  export: function(visual_data, filename) {
+    var self = this;
+    if (!filename) { filename = this.get('name') + ".n3"; }
+    data = { 
+      token: App.Auth.get('session.token'),
+      visual_data: visual_data,
+    };
+    url = "/social_networks/" + this.get('id') + ".rdf";
+    $.get(url, data).then(function(response) {
+      console.log(response);
+      var a = document.createElement('a');
+      var blob = new Blob([response], {'type':'application\/octet-stream'});
+      a.href = window.URL.createObjectURL(blob);
+      a.download = filename;
+      a.click();
+    }, function(event) {
+      self.set('errorMessage', "Something went wrong");
+    });
+  },
+
+  exportVisual: function() {
+    this.export(1, this.get('name') + "_visual.n3");
+  },
 });
