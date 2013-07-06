7d3e2135c81b417fb9c1c52af63e0c26610d2764
Felipe Espinoza <fespinozacast@gmail.com>
2013-07-05 23:30:33 -0400

    ADD: host config option for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,6 @@ EmberSge::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.ember.variant = :production
+
+  config.action_mailer.default_url_options = { :host => 'social-graph-editor.heroku.com' }
 end
