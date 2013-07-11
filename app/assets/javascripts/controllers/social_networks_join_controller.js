797f702c796d81c37f30873244eef0c9f2ec99ba
Felipe Espinoza <fespinozacast@gmail.com>
2013-07-11 11:56:51 -0400

    ADD: transition to second step in join SN

--- app/assets/javascripts/controllers/social_networks_join_controller.js
+++ app/assets/javascripts/controllers/social_networks_join_controller.js
@@ -1,4 +1,20 @@
 App.SocialNetworksJoinController = Ember.Controller.extend({
   needs: ['social_networks'],
+  currentStep: 1,
+  originalSocialNetwork: null,
+
+  next: function() {
+    this.set('currentStep', this.get('currentStep') + 1);
+  },
+
+  isFirstStep: function() {
+    return this.get('currentStep') == 1;
+  }.property('currentStep'),
+
+  cancel: function() {
+    this.set('currentStep', 1);
+    this.set('originalSocialNetwork', null);
+    this.transitionToRoute('social_networks.index');
+  },
 
 });
