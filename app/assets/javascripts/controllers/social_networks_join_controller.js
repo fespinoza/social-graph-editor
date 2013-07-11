54e788ec3a28dfb8b0ac4ae5c52a796ba8d933ce
Felipe Espinoza <fespinozacast@gmail.com>
2013-07-11 13:15:32 -0400

    ADD: importing and functional step 1 in join wizard

--- app/assets/javascripts/controllers/social_networks_join_controller.js
+++ app/assets/javascripts/controllers/social_networks_join_controller.js
@@ -2,9 +2,51 @@ App.SocialNetworksJoinController = Ember.Controller.extend({
   needs: ['social_networks'],
   currentStep: 1,
   originalSocialNetwork: null,
+  errorMessage: null,
+  successMessage: null,
+  importedSocialNetwork: null,
+  file: null,
+  fileContents: null,
+
+  reset: function() {
+    this.set('currentStep', 1);
+    this.set('originalSocialNetwork', null);
+    this.set('errorMessage', null);
+    this.set('successMessage', null);
+    this.set('file', null);
+    this.set('fileContents', null);
+    if (this.get('importedSocialNetwork')) {
+      this.get('importedSocialNetwork').deleteRecord();
+      this.set('importedSocialNetwork', null);
+    }
+  },
 
   next: function() {
-    this.set('currentStep', this.get('currentStep') + 1);
+    var self = this;
+    data = { 
+      content: this.get('fileContents'),
+      token: App.Auth.get('session.token'),
+    };
+    $("body").addClass("loading");
+    $.post('/social_networks/import.json', data).then(
+      function(response) {
+        console.log(response);
+        socialNetwork = App.SocialNetwork.find(response.social_network.id);
+        socialNetwork.set('temporal', true);
+        self.set('importedSocialNetwork', socialNetwork);
+        self.set('file', null);
+        self.set('fileContents', null);
+        self.set('successMessage', "Social Network imported successfully");
+        self.set('currentStep', self.get('currentStep') + 1);
+        $("body").removeClass("loading");
+      },
+      function(event) {
+        message = "It wasn't imported because a incorrect file " +
+                  "format or RDF/N3 file invalid";
+        self.set('errorMessage', message);
+        $("body").removeClass("loading");
+      }
+    );
   },
 
   isFirstStep: function() {
@@ -12,8 +54,7 @@ App.SocialNetworksJoinController = Ember.Controller.extend({
   }.property('currentStep'),
 
   cancel: function() {
-    this.set('currentStep', 1);
-    this.set('originalSocialNetwork', null);
+    this.reset();
     this.transitionToRoute('social_networks.index');
   },
 
