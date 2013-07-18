0d3da174ccc7ce7b57ae601e12c5244a785cd0a5
Felipe Espinoza <fespinozacast@gmail.com>
2013-07-18 12:54:56 -0400

    ADD: make families always clickable, closes #26

--- app/assets/javascripts/views/family_view.js
+++ app/assets/javascripts/views/family_view.js
@@ -4,40 +4,27 @@ App.FamilyView = Ember.View.extend({
   lastMode: null,
 
   didInsertElement: function() {
-    this.showEnabledOrDisabled();
+    this.$().addClass('enabled')
+    this.$("span.label").css("background-color", this.get('content.color'));
   },
 
   click: function() {
     console.log("clicked on family "+this.get('content.name'));
-    if(this.$().hasClass('enabled')) {
-      if(this.$().hasClass('selected')) {
-        this.clearSelected();
-      } else {
-        $("#families .family").removeClass('selected');
-        this.$().addClass('selected');
-        this.get('controller').send('selectFamily', this.get('content'));
-      }
+    if(this.$().hasClass('selected')) {
+      this.clearSelected();
+    } else {
+      $("#families .family").removeClass('selected');
+      this.$().addClass('selected');
+      this.get('controller').send('selectFamily', this.get('content'));
+      this.set('controller.socialNetwork.currentMode', this.get('content.kind'));
     }
   },
 
-  enableOnRigthMode: function(){
+  clearSelectionOnModeChange: function(){
     mode = this.get('controller.socialNetwork.currentMode');
     if (this.get('lastMode') != mode) { this.clearSelected(); }
-    this.showEnabledOrDisabled();
     this.set('lastMode', mode);
   }.observes('controller.socialNetwork.currentMode'),
-
-  showEnabledOrDisabled: function() {
-    kind = this.get('content.kind');
-    mode = this.get('controller.socialNetwork.currentMode');
-    if (kind === mode || mode === "Hand") {
-      this.$().addClass('enabled')
-      this.$("span.label").css("background-color", this.get('content.color'));
-    } else {
-      this.$().removeClass('enabled');
-      this.$("span.label").css("background-color", "");
-    }
-  },
 
   clearSelected: function() {
     this.$().removeClass('selected');
