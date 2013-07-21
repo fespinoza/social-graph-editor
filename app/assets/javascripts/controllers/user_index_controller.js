85ff81437cd5b0fc5ac089cc912c8b3409a44796
Felipe Espinoza <fespinozacast@gmail.com>
2013-07-21 17:03:51 -0400

    FIX: remove unused log messages

--- app/assets/javascripts/controllers/user_index_controller.js
+++ app/assets/javascripts/controllers/user_index_controller.js
@@ -2,7 +2,6 @@ App.UserIndexController = Ember.ObjectController.extend({
   errorMessage: null,
 
   startEditing: function() {
-    console.log("user index: start editing");
     // create a new record on a local transaction
     this.transaction = this.get('store').transaction();
     this.set('content', this.transaction.createRecord(App.User, {}));
@@ -10,7 +9,6 @@ App.UserIndexController = Ember.ObjectController.extend({
   },
 
   register: function() {
-    console.log("user index: register");
     validParams = this.get('email') != null &&
                   this.get('password') != null &&
                   this.get('password') == this.get('password_confirmation');
@@ -21,12 +19,13 @@ App.UserIndexController = Ember.ObjectController.extend({
       this.set('errorMessage', null);
       // TODO: show email duplication error
     } else {
-      this.set('errorMessage', 'Email and Password must be presetent and Password and Password Confirmation must be equal');
+      message = "Email and Password must be presetent and " +
+                "Password and Password Confirmation must be equal";
+      this.set('errorMessage', message);
     }
   },
 
   stopEditing: function() {
-    console.log("user index: stop editing");
     // rollback the local transaction if it hasn't already been cleared
     if (this.transaction) {
       this.transaction.rollback();
