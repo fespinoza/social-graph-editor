9b945ac60e70dd4045423dda1975feefb85f067c
Felipe Espinoza <fespinozacast@gmail.com>
2013-07-21 22:16:57 -0400

    ADD: handle case when user can't be updated

--- app/assets/javascripts/controllers/user_edit_controller.js
+++ app/assets/javascripts/controllers/user_edit_controller.js
@@ -1,8 +1,12 @@
 App.UserEditController = Ember.ObjectController.extend({
-  reset: function() {
-    this.transaction = null;
+  resetMessages: function() {
     this.set('errorMessage', null);
     this.set('successMessage', null);
+  },
+
+  reset: function() {
+    this.resetMessages();
+    this.transaction = null;
     this.set('password', null);
     this.set('newPassword', null);
     this.set('newPasswordConfirmation', null);
@@ -18,10 +22,20 @@ App.UserEditController = Ember.ObjectController.extend({
     if (this.get('newPassword') == this.get('newPasswordConfirmation')) {
       var controller = this;
       this.get('content').on('becameError', function() {
+        // handle error case
+        // this is a manual fix because apparently ember-data does not implement
+        // how to handle errors
+        // more info here
+        // http://stackoverflow.com/questions/14741377/what-can-you-do-with-ember-data-models-when-in-the-error-state
+        controller.transaction = null;
+        controller.get('stateManager').transitionTo('loaded.updated');
+        controller.startEditing();
+
         controller.set('errorMessage', "Current Password was mistaken");
       });
       this.get('content').on('didUpdate', function() {
         controller.set('successMessage', "User successfully updated");
+        controller.transitionToRoute('social_networks.index');
       });
       this.transaction.commit();
     } else {
--- app/assets/javascripts/routes/user_edit_route.js
+++ app/assets/javascripts/routes/user_edit_route.js
@@ -1,11 +1,9 @@
 App.UserEditRoute = Ember.Route.extend({
   model: function(params) {
-    console.log("user edit model");
     return App.User.find(params.user_id);
   },
 
   setupController: function(controller, model) {
-    console.log("setup controller");
     controller.set('content', model);
     controller.startEditing();
   },
