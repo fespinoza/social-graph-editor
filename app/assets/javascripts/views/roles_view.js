660d98beb0d5b1d38d271fe9a9eac64fb763ce3a
Felipe Espinoza <fespinozacast@gmail.com>
2013-08-19 23:56:34 -0400

    ADD: text shadow to role names

--- app/assets/javascripts/views/roles_view.js
+++ app/assets/javascripts/views/roles_view.js
@@ -18,11 +18,14 @@ App.RolesView = Ember.View.extend({
     svg = d3.select("#graph_canvas .root");
     data = this.get('controller.content').toArray();
     this.roleLine = svg.selectAll("line.role").data(data);
+    this.roleTextShadow = svg.selectAll("text.role-shadow").data(data);
     this.roleText = svg.selectAll("text.role").data(data);
     // enter
     this.roleLine.enter().append("line")
         .classed('role', true)
         .on('click', this.roleClick());
+    this.roleTextShadow.enter().append("text")
+        .classed('role-shadow', true);
     this.roleText.enter().append("text")
         .classed('role', true)
         .on('click', this.roleClick());
@@ -30,6 +33,7 @@ App.RolesView = Ember.View.extend({
     this.tick();
     // exit
     this.roleLine.exit().remove();
+    this.roleTextShadow.exit().remove();
     this.roleText.exit().remove();
   }.observes('controller.length'),
 
@@ -40,6 +44,10 @@ App.RolesView = Ember.View.extend({
       .attr('x2', function(d) { return d.get('x2'); })
       .attr('y2', function(d) { return d.get('y2'); })
       .style('marker-end', 'url(#end-arrow)' );
+    this.roleTextShadow
+      .text(function(d) { return d.get('name'); })
+      .attr('x', function(d) { return d.get('text_x'); })
+      .attr('y', function(d) { return d.get('text_y'); });
     this.roleText
       .text(function(d) { return d.get('name'); })
       .attr('x', function(d) { return d.get('text_x'); })
