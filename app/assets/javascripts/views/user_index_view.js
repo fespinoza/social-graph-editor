6eb14261329d8ac88c7470313f0eaea766afa457
Felipe Espinoza <fespinozacast@gmail.com>
2013-09-29 23:35:08 -0300

    ADD: modular validation functions to user index view

--- app/assets/javascripts/views/user_index_view.js
+++ app/assets/javascripts/views/user_index_view.js
@@ -2,23 +2,74 @@ App.UserIndexView = Ember.View.extend({
 
   submit: function() {
     console.log("register");
-    var isValid = true;
-    isValid = this.showErrorIfUndefined("Email", "#newUserEmail") && isValid;
-    isValid = this.showErrorIfUndefined("Password", "#newUserPassword") && isValid;
-    isValid = this.showErrorIfUndefined("PasswordConfirmation", "#newUserPasswordConfirmation") && isValid;
 
+    var isValid = true;
+    isValid = this.validatesPresenceOf("Email", "#newUserEmail") && isValid;
+    isValid = this.validatesEmailFormat("Email", "#newUserEmail") && isValid;
+    isValid = this.validatesPresenceOf("Password", "#newUserPassword") && isValid;
+    isValid = this.validatesMinimunLengthOf("Password", "#newUserPassword", 6) && isValid;
+    isValid = this.validatesEqualityBetween("PasswordConfirmation", "Password", "#newUserPasswordConfirmation") && isValid;
+    
     if (isValid) {
-      //this.get('controller').send('register');
-      console.log("invalid!");
-    } else {
       console.log("valid!");
+      this.get('controller').send('register');
+    } else {
+      console.log("invalid!");
     }
   },
 
-  showErrorIfUndefined: function(fieldName, fieldId) {
-    var attribute = this.get('content.'+fieldName.toLowerCase()),
-        message   = fieldName + " must not be empty";
-    if(attribute != "" && attribute != null) {
+  // external interface of validators
+  validatesPresenceOf: function(fieldName, fieldId) {
+    var attribute = this.getAttributeValue(fieldName),
+        message = fieldName + " must be present";
+    return this.validate(fieldName, fieldId, attribute, this.validations.presence, message);
+  },
+
+  validatesEmailFormat: function (fieldName, fieldId) {
+    var attribute = this.getAttributeValue(fieldName),
+        message = fieldName + " must be a valid email";
+    return this.validate(fieldName, fieldId, attribute, this.validations.emailFormat, message);
+  },
+
+  validatesMinimunLengthOf: function(fieldName, fieldId, min) {
+    var attribute = this.getAttributeValue(fieldName),
+        message = fieldName + " must be at least "+min+" characters long";
+    validator = this.validations.minLength(min);
+    return this.validate(fieldName, fieldId, attribute, validator, message);
+  },
+
+  validatesEqualityBetween: function(fieldName, secondFieldName, fieldId) {
+    var attribute = this.getAttributeValue(fieldName),
+        message = fieldName + " must match with "+secondFieldName,
+        secondAttribute = this.getAttributeValue(secondFieldName);
+    validator = this.validations.equalsTo(secondAttribute);
+    return this.validate(fieldName, fieldId, attribute, validator, message);
+
+  },
+
+  // logic to extract attribute value from name
+  getAttributeValue: function(fieldName) {
+    var field = fieldName.replace(/([A-Z])/g, function($1){return "_"+$1.toLowerCase();}).replace(/^_/, '');
+    return this.get('controller.' + field);
+  },
+
+  // generic pure validations logic
+  validations: {
+    presence: function(attribute) { return attribute != "" && attribute != null; },
+    emailFormat: function(attribute) { return /^.+@.+\..+$/.test(attribute); },
+    minLength: function(min) { 
+      return function(attribute) { 
+        return attribute != null && attribute.length >= min;
+      }
+    },
+    equalsTo: function(comparisonValue) {
+      return function(attribute) { return attribute === comparisonValue }
+    }
+  },
+
+  // generic validation function according to bootstrap
+  validate: function (fieldName, fieldId, attribute, validator, message) {
+    if(validator(attribute)) {
       this.$(fieldId).parents(".control-group").removeClass("error");
       this.$(fieldId).siblings(".help-inline").remove();
       return true;
@@ -31,4 +82,5 @@ App.UserIndexView = Ember.View.extend({
       return false;
     }
   },
+
 });
